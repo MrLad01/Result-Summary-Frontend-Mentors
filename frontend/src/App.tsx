7210754959fbcc8aa9c2c012bafee7250e32bcff
MrLad01 <ladayodeji@gmail.com>
2023-09-27 16:09:26 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,9 @@ function App() {
     <>
       <div className="h-screen w-screen flex items-center justify-center">
         <div className="lg:flex lg:w-1/3 lg:h-1/2 sm:w-screen sm:h-screen sm:grid" >
-          <div className="lg:w-1/2 rounded-2xl shadow-2xl" style={{backgroundColor: "blue"}}></div>
+          <div className="lg:w-1/2 rounded-2xl shadow-2xl flex justify-center items-center" style={{backgroundColor: "hsl(252, 100%, 67%)"}}>
+            <div className="h-1/3 w-1/2 rounded-full bg-slate-700"></div>
+          </div>
           <div className="lg:w-1/2 rounded-e-2xl shadow-2xl"></div>
         </div>
       </div>
