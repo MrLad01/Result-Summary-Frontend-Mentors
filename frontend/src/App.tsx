1afe682582bb4bafec1d76338be6808671203ebd
MrLad01 <ladayodeji@gmail.com>
2023-09-27 15:56:09 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import hello from "../results-summary-component-main/assets/images/icon-memory.svg"
 
 
 function App() {
   return (
     <>
-      <div className="flex lg:w-1/2 lg:h-80 sm:w-screen sm:h-screen sm:grid" style={{backgroundColor: "blue"}}>
-        Hello World
+      <div className="h-screen w-screen flex items-center justify-center">
+        <div className="lg:flex lg:w-1/3 lg:h-1/2 sm:w-screen sm:h-screen sm:grid" >
+          <div className="lg:w-1/2 rounded-2xl shadow-2xl" style={{backgroundColor: "blue"}}></div>
+          <div className="lg:w-1/2 rounded-e-2xl shadow-2xl"></div>
+        </div>
       </div>
     </>
   )
