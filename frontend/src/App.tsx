7f7f5f8797bcfefe9b1d7a964d379858e26813b7
MrLad01 <ladayodeji@gmail.com>
2023-09-28 08:58:42 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import hello from "../results-summary-component-main/assets/images/icon-memory.svg"
+import Memory  from "../results-summary-component-main/assets/images/icon-memory.svg"
 import data from "../results-summary-component-main/data.json"
 
 
@@ -22,16 +20,18 @@ function App() {
     <>
       <div className="h-screen w-screen flex items-center justify-center">
         <div className="sm:max-2xl:flex sm:max-2xl:w-1/2 sm:max-2xl:h-3/5 xs:max-sm:w-screen xs:max-sm:h-screen xs:max-sm:grid" >
-          <div className="sm:max-2xl:w-1/2 sm:max-2xl:h-full xs:max-sm:h-1/2 xs:max-sm:w-full sm:max-2xl:rounded-2xl shadow-2xl grid justify-center items-center bg-gradient-to-b from-background1 to-background2">
+          <div className="sm:max-2xl:w-1/2 sm:max-2xl:h-full xs:max-sm:h-1/2 xs:max-sm:w-full sm:max-2xl:rounded-2xl shadow-2xl flex flex-col justify-center items-center bg-gradient-to-b from-background1 to-background2 p-2">
             <h3>Your Result</h3>
-            <div className="h-full w-full rounded-full bg-gradient-to-b from-circle1 to-circle2 grid justify-center items-center gap-0">
+            <div className="h-1/3 w-2/5 rounded-full bg-gradient-to-b from-circle1 to-circle2 grid justify-center items-center gap-0">
              <span>{score}</span>
              <span>of 100</span>
             </div>
             <div>{score < 50 ? "You failed! Please Try Again": "Great"}</div>
             <div>{score > 70 ? "You scored higher than 65% of the people who have taken these test": "Well done"}</div>
             </div>
-          <div className="sm:max-2xl:w-1/2 rounded-e-2xl shadow-2xl"></div>
+          <div className="sm:max-2xl:w-1/2 rounded-e-2xl shadow-2xl">
+            <Memory />
+          </div>
         </div>
       </div>
     </>
