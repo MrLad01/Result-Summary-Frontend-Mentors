8aeb0dae871c5ef27fe227c01245d24a245196dc
MrLad01 <ladayodeji@gmail.com>
2023-09-28 14:32:35 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,16 +16,16 @@ function App() {
     <>
       <div className="h-screen w-screen flex items-center justify-center">
         <div className="sm:max-2xl:flex sm:max-2xl:w-1/2 sm:max-2xl:h-3/5 xs:max-sm:w-screen xs:max-sm:h-full xs:max-sm:grid" >
-          <div className="sm:max-2xl:w-1/2 sm:max-2xl:h-full xs:max-sm:h-full xs:max-sm:w-full xs:max-sm:rounded-b-3xl sm:max-2xl:rounded-2xl shadow-2xl flex flex-col justify-center items-center bg-gradient-to-b from-background1 to-background2 p-2">
-            <header className="text-center">Your Result</header>
-            <div className="h-1/3 w-2/5 xs:max-sm:w-1/5 xs:max-sm:h-full rounded-full bg-gradient-to-b from-circle1 to-circle2 grid justify-center items-center gap-0 p-4">
-             <span className="font-extrabold text-5xl text-center">{score}</span>
-             <span className="text-center">of 100</span>
+          <div className="sm:max-2xl:w-1/2 sm:max-2xl:h-full xs:max-sm:h-full xs:max-sm:w-full xs:max-sm:rounded-b-3xl sm:max-2xl:rounded-2xl shadow-2xl flex flex-col justify-center items-center bg-gradient-to-b from-background1 to-background2 p-2 text-white">
+            <header className="text-center opacity-80">Your Result</header>
+            <div className="h-fit w-1/3 xs:max-sm:w-1/3 xs:max-sm:h-auto rounded-full bg-gradient-to-b from-circle1 to-circle2 grid justify-center items-center gap-0 p-4 my-6">
+             <span className="font-bold text-5xl text-center">{score}</span>
+             <span className="text-center opacity-80">of 100</span>
             </div>
-            <div className="text-xl font-bold">
+            <div className="text-xl font-bold mb-4">
               {score < 50 ? "You failed! Please Try Again": "Great"}
             </div>
-            <div className="xs:max-sm:max-w-xs text-center">
+            <div className="xs:max-sm:max-w-xs text-center opacity-80">
               {score > 70 ? "You scored higher than 65% of the people who have taken these test": "Well done"}
             </div>
             </div>
