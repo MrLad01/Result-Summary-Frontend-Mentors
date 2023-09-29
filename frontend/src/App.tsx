f976bd0696998fcad887d79597132ccfcf274698
MrLad01 <ladayodeji@gmail.com>
2023-09-29 16:54:06 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,7 +33,7 @@ function App() {
       <div className="h-screen w-screen flex items-center justify-center">
         <div className="sm:max-2xl:flex sm:max-2xl:w-1/2 sm:max-2xl:h-3/5 xs:max-sm:w-screen xs:max-sm:h-full xs:max-sm:grid" >
           <div className="sm:max-2xl:w-1/2 sm:max-2xl:h-full xs:max-sm:h-full xs:max-sm:w-full xs:max-sm:rounded-b-3xl sm:max-2xl:rounded-2xl shadow-2xl flex flex-col justify-center items-center bg-gradient-to-b from-background1 to-background2 p-2 text-white">
-            <header className="text-center opacity-70"  role="banner">Your Result</header>
+            <header className="text-center opacity-70"  >Your Result</header>
             <div className="h-fit w-1/3 xs:max-sm:w-1/3 xs:max-sm:h-auto rounded-full bg-gradient-to-b from-circle1 to-circle2 grid justify-center items-center gap-0 p-4 my-6">
              <span className="font-bold text-5xl text-center">{score}</span>
              <span className="text-center opacity-70">of 100</span>
